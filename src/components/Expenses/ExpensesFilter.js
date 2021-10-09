aff9e06fa2880b7bd419fa293aefe93e26bf2595
Bilal Ahmed <bilalahmedim@gmail.com>
2021-10-09 14:33:20 +0530

    instructor solution with two way binding

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,19 +1,15 @@
-import React, { useState } from "react";
-
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-  const [enteredYear, setEnteredYear] = useState("");
-
-  const yearChangeHandler = (event) => {
-    setEnteredYear(event.target.value);
-    props.onSaveFilterData(enteredYear);
+  const dropDownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
   };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange={yearChangeHandler}>
+        <select onChange={dropDownChangeHandler} value={props.selected}>
+          //value={props.selected} is two way binding
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
